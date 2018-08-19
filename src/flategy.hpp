9411a5cfde5e0213a83e2a79796a3c8d1422860e
Douglas Orr <douglas.orr99@gmail.com>
2018-08-19 20:22:02 +0100

    Native basic scaffolding for C extension library

---
+++ src/flategy.hpp
@@ -0,0 +1,10 @@
+#ifndef FLATEGY_HPP
+#define FLATEGY_HPP
+
+namespace flategy {
+
+    extern const char* Docstring;
+
+} // namespace flategy
+
+#endif // FLATEGY_HPP
---
+++ src/libflategy.cpp
@@ -0,0 +1,42 @@
+#include "Python.h"
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include "numpy/arrayobject.h"
+#include "flategy.hpp"
+
+namespace flategy {
+
+    const char* Docstring = "Core native logic for the RTS game Flategy";
+
+} // namespace flategy
+
+namespace {
+
+    PyObject* hello_world(PyObject*, PyObject*) {
+        return PyUnicode_FromString("hello from C++");
+    }
+
+    PyMethodDef methods[] = {
+        { "hello_world", hello_world, METH_VARARGS,
+          "Say hello from native code" },
+        { nullptr, nullptr, 0, nullptr }
+    };
+
+    struct PyModuleDef module = {
+        PyModuleDef_HEAD_INIT,
+        "libflategy",
+        flategy::Docstring,
+        -1,
+        methods,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr
+    };
+
+
+} // namespace (anonymous)
+
+PyMODINIT_FUNC PyInit_libflategy() {
+    import_array();
+    return PyModule_Create(&module);
+}
