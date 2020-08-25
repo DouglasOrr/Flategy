a792fdae11c5898502221ff4b749b2ce17daaf8f
Douglas Orr <douglas.orr99@gmail.com>
2020-08-25 22:24:38 +0100

    Add Grid abstraction, refactor Map

--- src/map.rs
+++ src/map.rs
@@ -1,9 +1,8 @@
 /// Representation and generation of maps (fixed terrain)
-use super::core;
+use super::{core, util};
 use itertools::Itertools;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
-use std::ops::{Index, IndexMut};
 
 #[derive(Debug, Copy, Clone)]
 enum Tile {
@@ -14,61 +13,45 @@ enum Tile {
 }
 
 struct Map {
-    width: u16,
-    height: u16,
-    cells: Vec<Tile>,
-}
-
-impl Index<(u16, u16)> for Map {
-    type Output = Tile;
-
-    fn index(&self, index: (u16, u16)) -> &Self::Output {
-        let index = self.get_index(index.0, index.1);
-        &self.cells[index]
-    }
-}
-
-impl IndexMut<(u16, u16)> for Map {
-    fn index_mut(&mut self, index: (u16, u16)) -> &mut Self::Output {
-        let index = self.get_index(index.0, index.1);
-        &mut self.cells[index]
-    }
+    cells: util::Grid<(usize, usize), Tile>,
 }
 
 impl Map {
-    fn get_index(&self, x: u16, y: u16) -> usize {
-        (y as usize) * (self.width as usize) + (x as usize)
+    fn width(&self) -> usize {
+        self.cells.shape().0
+    }
+
+    fn height(&self) -> usize {
+        self.cells.shape().1
     }
 
     fn generate(seed: u64) -> Map {
         let mut rng = StdRng::seed_from_u64(seed);
 
         // Overall map
-        let width = rng.gen_range(10, 15);
-        let height = rng.gen_range(10, 15);
-        let count = (height as usize) * (width as usize);
         let mut map = Map {
-            width: width,
-            height: height,
-            cells: [Tile::Empty].repeat(count),
+            cells: util::Grid::new((rng.gen_range(10, 15), rng.gen_range(10, 15)), &Tile::Empty),
         };
 
         // Spawn points
-        let spawn_x = rng.gen_range(0, width / 3);
-        let spawn_y = rng.gen_range(0, height / 3);
-        map[(spawn_x, spawn_y)] = Tile::Spawn(core::PlayerID { index: 0 });
-        map[(width - 1 - spawn_x, height - 1 - spawn_y)] = Tile::Spawn(core::PlayerID { index: 1 });
+        let spawn_0 = (
+            rng.gen_range(0, map.width() / 3),
+            rng.gen_range(0, map.height() / 3),
+        );
+        map.cells[spawn_0] = Tile::Spawn(core::PlayerID { index: 0 });
+        let spawn_1 = (map.width() - 1 - spawn_0.0, map.height() - 1 - spawn_0.1);
+        map.cells[spawn_1] = Tile::Spawn(core::PlayerID { index: 1 });
 
         // Food
         map.generate_tiles(
-            rng.gen_range(count / 20, count / 10),
+            rng.gen_range(map.cells.len() / 20, map.cells.len() / 10),
             || Tile::Food(core::Food { amount: 100 }),
             &mut rng,
         );
 
         // Walls
         map.generate_tiles(
-            rng.gen_range(count / 10, count / 4),
+            rng.gen_range(map.cells.len() / 10, map.cells.len() / 4),
             || Tile::Wall,
             &mut rng,
         );
@@ -80,9 +63,11 @@ impl Map {
     where
         F: Fn() -> Tile,
     {
-        let count = self.cells.len();
         for _ in 0..max {
-            let index = rng.gen_range(0, count);
+            let index = (
+                rng.gen_range(0, self.width()),
+                rng.gen_range(0, self.height()),
+            );
             match self.cells[index] {
                 Tile::Empty => self.cells[index] = generate(),
                 _ => (),
@@ -92,7 +77,8 @@ impl Map {
 
     fn dump(&self) -> String {
         self.cells
-            .chunks(self.width as usize)
+            .data()
+            .chunks(self.width())
             .map(|chunk| {
                 chunk
                     .iter()
@@ -116,7 +102,7 @@ mod tests {
     #[test]
     fn test_map() {
         let map = Map::generate(75);
-        assert!((map.width * map.height) as usize == map.cells.len());
+        assert!(map.width() * map.height() == map.cells.len());
 
         let dump = map.dump();
         println!("{}", dump);
--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,19 @@
 /// General-purpose utilities
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
+
+// VecMap
 
 /// A mapping from index to optional items, implemented using a Vec.
 ///
 /// The mapping grows to hold the max index added.
 ///
 #[derive(Clone, Default)]
-pub struct VecMap<T: Clone> {
+pub struct VecMap<T> {
     items: Vec<Option<T>>,
 }
 
 impl<T: Clone> VecMap<T> {
-    pub fn new() -> VecMap<T> {
+    pub fn new() -> Self {
         VecMap { items: Vec::new() }
     }
 
@@ -23,7 +25,7 @@ impl<T: Clone> VecMap<T> {
     }
 }
 
-impl<T: Clone> Index<usize> for VecMap<T> {
+impl<T> Index<usize> for VecMap<T> {
     type Output = Option<T>;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -34,6 +36,105 @@ impl<T: Clone> Index<usize> for VecMap<T> {
         }
     }
 }
+
+// GridIndex
+
+/// A type that can be used as a multidimensional index into a Grid
+pub trait GridIndex: Copy + std::fmt::Debug {
+    fn size(self) -> usize;
+    fn in_bounds(self, index: Self) -> bool;
+    fn offset(self, index: Self) -> usize;
+}
+
+impl GridIndex for (usize, usize) {
+    fn size(self) -> usize {
+        self.0 * self.1
+    }
+    fn in_bounds(self, index: Self) -> bool {
+        index.0 < self.0 && index.1 < self.1
+    }
+    fn offset(self, index: Self) -> usize {
+        index.0 + index.1 * self.0
+    }
+}
+
+impl GridIndex for (usize, usize, usize) {
+    fn size(self) -> usize {
+        self.0 * self.1 * self.2
+    }
+    fn in_bounds(self, index: Self) -> bool {
+        index.0 < self.0 && index.1 < self.1 && index.2 < self.2
+    }
+    fn offset(self, index: Self) -> usize {
+        index.0 + index.1 * self.0 + index.2 * self.0 * self.1
+    }
+}
+
+// Grid
+
+/// An N-Dimensional grid of items, stored flattened in a Vec
+pub struct Grid<I, T> {
+    shape: I,
+    data: Vec<T>,
+}
+
+impl<I, T> Grid<I, T>
+where
+    I: GridIndex,
+    T: Clone,
+{
+    pub fn new(shape: I, item: &T) -> Self {
+        let data = (0..shape.size()).map(|_| item.clone()).collect();
+        Grid {
+            shape: shape,
+            data: data,
+        }
+    }
+}
+
+impl<I, T> Grid<I, T>
+where
+    I: GridIndex,
+{
+    pub fn shape(&self) -> I {
+        self.shape
+    }
+    pub fn data(&self) -> &Vec<T> {
+        &self.data
+    }
+    pub fn len(&self) -> usize {
+        self.shape.size()
+    }
+    fn checked_offset(&self, index: I) -> usize {
+        if !self.shape.in_bounds(index) {
+            panic!("index {:?} out of bounds for shape {:?}", index, self.shape);
+        }
+        self.shape.offset(index)
+    }
+}
+
+impl<I, T> Index<I> for Grid<I, T>
+where
+    I: GridIndex,
+{
+    type Output = T;
+
+    fn index(&self, index: I) -> &Self::Output {
+        &self.data[self.checked_offset(index)]
+    }
+}
+
+impl<I, T> IndexMut<I> for Grid<I, T>
+where
+    I: GridIndex,
+{
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        let offset = self.checked_offset(index);
+        &mut self.data[offset]
+    }
+}
+
+// Tests
 
 #[cfg(test)]
 mod tests {
@@ -47,5 +148,43 @@ mod tests {
         map.insert(1, Some('a'));
         assert_eq!(None, map[0]);
         assert_eq!(Some('a'), map[1]);
+    }
+
+    #[test]
+    fn test_grid_2d() {
+        let mut grid = Grid::new((2, 3), &'a');
+        assert_eq!(6, grid.len());
+        assert_eq!((2, 3), grid.shape());
+        assert_eq!('a', grid[(1, 2)]);
+
+        grid[(1, 2)] = 'b';
+        assert_eq!('b', grid[(1, 2)]);
+        assert_eq!('a', grid[(1, 1)]);
+    }
+
+    #[test]
+    fn test_grid_3d() {
+        let mut grid = Grid::new((2, 3, 4), &'a');
+        assert_eq!(24, grid.len());
+        assert_eq!((2, 3, 4), grid.shape());
+        assert_eq!('a', grid[(1, 2, 3)]);
+
+        grid[(1, 2, 3)] = 'b';
+        assert_eq!('b', grid[(1, 2, 3)]);
+        assert_eq!('a', grid[(0, 2, 3)]);
+    }
+
+    #[test]
+    fn test_grid_no_overlap() {
+        // Make sure each index maps to a unique location
+        let mut grid = Grid::new((5, 6, 7), &false);
+        for i in 0..(grid.shape.0) {
+            for j in 0..(grid.shape.1) {
+                for k in 0..(grid.shape.2) {
+                    assert!(!grid[(i, j, k)]);
+                    grid[(i, j, k)] = true;
+                }
+            }
+        }
     }
 }
